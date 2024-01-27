2e04990d3c6cbb17161c8c386b6cb9c8bf741fa7
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2024-01-27 00:40:50 -0800

    Created SoundByte Scriptable Object

---
+++ Global Game Jam 2024/Assets/Scripts/Scriptable Objects/SoundByte.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class SoundByte : ScriptableObject
+{
+    public AudioClip audio;
+    [TextArea(15, 20)] public string text;
+    [Range(0f, 3f)] public float startDelay;
+    [Range(0f, 3f)] public float endDelay;
+}
