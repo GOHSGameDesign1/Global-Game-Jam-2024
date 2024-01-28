b037344a7d4840caff2532c42a8f7f16355e56a2
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2024-01-27 17:47:55 -0800

    Made SoundByte play into another one

--- Global Game Jam 2024/Assets/Scripts/ByteManager.cs
+++ Global Game Jam 2024/Assets/Scripts/ByteManager.cs
@@ -11,23 +11,35 @@ public class ByteManager : MonoBehaviour
 
     public SoundByte line;
 
+    public SoundByte[] lines;
+
+    public int index;
+
 
     private void Awake()
     {
-        source.clip = line.clip; 
+
     }
     // Start is called before the first frame update
     void Start()
     {
-        tmp.text = line.text;
         Play();
     }
 
     void Play()
     {
+        if (index >= lines.Length) return;
+        SoundByte currentLine = lines[index];
+        source.clip = currentLine.clip;
+        tmp.text = currentLine.text;
         ToggleText();
         Invoke("ToggleText", source.clip.length); // Disables subtitles once audio clip ends
+        if (currentLine.playNext)
+        {
+            Invoke("Play", source.clip.length);
+        }
         source.Play();
+        index++;
     }
 
     void ToggleText()
