42086c3d97c5ddf9f7b63b678a1661fcd138b30e
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2024-02-06 22:30:36 -0800

    Added Explosion Particles

--- Global Game Jam 2024/Assets/Scripts/ExplodeClick.cs
+++ Global Game Jam 2024/Assets/Scripts/ExplodeClick.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class ExplodeClick : MonoBehaviour, IClickable
 {
+    public ParticleSystem explodeParticles1;
+    public ParticleSystem explodeParticles2;
+
     public void OnClick()
     {
         if (ByteManager.Instance.isPlaying) return;
-        AudioManager.instance.PlaySound("GunShot");
-        if (AudioManager.instance.GetSound("GunShot") != null) ByteManager.Instance.Invoke("Play", AudioManager.instance.GetSound("GunShot").clip.length);
-        Debug.Log("Clicked Triangle");
-        Destroy(gameObject);
+        if (AudioManager.instance.GetSound("Explode") != null) ByteManager.Instance.Invoke("Play", AudioManager.instance.GetSound("Explode").clip.length);
+        Explode();
+    }
+
+    private void Explode()
+    {
+        Instantiate(explodeParticles1, transform.position, Quaternion.Euler(0, 0, 0));
+        Instantiate(explodeParticles2, transform.position, Quaternion.Euler(0, 0, 0));
+
+        GetComponent<SpriteRenderer>().color = Color.clear;
     }
 }
